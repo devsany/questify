cb73f3fda0d125aca46c2fd90170dc008b5e8d92
devsany <sunnyrajbodhgaya19@gmail.com>
2024-09-29 17:41:26 +0530

    css of contact page done

--- src/compound/Contact.jsx
+++ src/compound/Contact.jsx
@@ -10,17 +10,23 @@ function Contact() {
       className="form"
       style={{ display: "felx", justifyContent: "center", width: "100%" }}
     >
-      <h2 className="text-4xl font-bold bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Contact to Developer
-      </h2>
+      <div className=" mt-10 md:flex md:justify-center md:mt-10 md:mb-10 text-center block">
+        <h2 className="text-4xl font-bold bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Contact to Developer
+        </h2>
+      </div>
 
-      <div className="main_form_field" style={{ display: "flex" }}>
-        <div>
+      <div
+        className="main_form_field justify-center"
+        style={{ display: "flex" }}
+      >
+        <div className="md:m-0 ml-10 mt-10">
           <form onSubmit={handleSubmit} style={{ width: "30rem" }}>
             <div className="inputField1 ">
               <label htmlFor="email">Email Address</label>
               <br />
               <input
+                className="w-[250px] md:w-[300px]"
                 style={{ padding: "5px", margin: "5px" }}
                 id="email"
                 type="email"
@@ -35,14 +41,18 @@ function Contact() {
                 errors={state.errors}
               />
             </div>
+            <label className="mt-5" htmlFor="message">
+              {" "}
+              Message
+            </label>
             <div className="inputField1">
-              <label htmlFor="message"> Message</label>
               <textarea
                 style={{ padding: "5px", margin: "5px" }}
                 id="message"
                 name="message"
                 placeholder="Enter your message"
-                rows={10}
+                rows={5}
+                cols={37}
               />
             </div>
             <div className="error">
@@ -61,7 +71,7 @@ function Contact() {
             </button>
           </form>
         </div>
-        <div>
+        <div className="md:block hidden">
           <img
             style={{ width: "28.5rem", marginLeft: "20px" }}
             src="https://img.freepik.com/free-vector/email-marketing-internet-chatting-24-hours-support_335657-3009.jpg"
